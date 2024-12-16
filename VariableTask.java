f66b5a3b266fc6426981d33a2974e315e3d6f7a6
mohamedsukiferdev <mohamedsukifer@gmail.com>
2024-12-16 16:57:07 +0530

    Day-1- part2

---
+++ VariableTask.java
@@ -0,0 +1,20 @@
+class VariableTask{
+public static void main(String [] args){
+int intNum = 32;
+byte bNum = 126;
+float fNum = 10.12F;
+double dNum = 10.2354D;
+boolean booleanNum = true ;
+long lNum = 9024547489L;
+char a = 'h';
+String str = " java";
+System.out.println("intNum :"+ intNum );
+System.out.println("bNum :"+ bNum );
+System.out.println("fNum :"+ fNum );
+System.out.println("dNum :"+ dNum );
+System.out.println("booleanNum :"+ booleanNum );
+System.out.println("lNum :"+ lNum );
+System.out.println("a :"+ a );
+System.out.println("str :"+ str );
+}
+}
