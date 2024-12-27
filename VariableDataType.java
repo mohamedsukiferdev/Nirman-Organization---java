770149600d765eb58b67f523481ab7c0d2aa8e23
mohamedsukiferdev <mohamedsukifer@gmail.com>
2024-12-27 15:53:27 +0530

    Day-5

---
+++ VariableDataType.java
@@ -0,0 +1,30 @@
+package day5;
+
+public class VariableDataType {
+	public static void main(String[] args) {
+		String name1 = "parthiban";
+		String name2 = "das";
+		String adjective1 = "sleepy";
+		String adjective2 = "streesfull";
+		String noun1 = "ballons";
+		String noun2 = "slippers";
+		String verb1 = "play7";
+		String noun3 = "footsteps";
+		String noun4 = "trees";
+		String adjective3 = "shocked";
+		String place1 = "the ground";
+		String noun5 = "yogurt";
+		String noun6 = "unicorns";
+		String number = "2066";
+
+		String story = "This morning " + name1 + " woke up feeling " + adjective1 + ".'It is going to be a "
+				+ adjective2 + " day!' Outside, a bunch of " + noun1 + "s were protesting to keep " + noun2
+				+ " in stores. They began to " + verb1 + " to the rhythm of the " + noun3 + ", which made all the "
+				+ noun4 + "s very " + adjective3 + ". Concerned, " + name1 + " texted " + name2 + ", who flew " + name1
+				+ " to " + place1 + " and dropped " + name1 + " in a puddle of frozen " + noun5 + ". " + name1
+				+ " woke up in the year " + number + ", in a world where " + noun6 + "s ruled the world.";
+
+		System.out.println(story);
+	}
+
+}
